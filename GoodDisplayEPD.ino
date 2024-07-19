6d0f98f8c858f66dd08ad74f624a202af1e9d943
AlexLazarovich <alexlazarovch@gmail.com>
2024-07-19 19:21:45 +0300

    Send by chunks

--- GoodDisplayEPD.ino
+++ GoodDisplayEPD.ino
@@ -11,39 +11,59 @@ const char* password = "l1aza9ro9vic4h";
 WebServer server(80);
 GDEP073E01 epdDisplay;
 
-unsigned char* encodedImage = nullptr;
-int encodedImageLength = 0;
+unsigned char* image = nullptr;
+int imageSize = 0;
+int chunkSize = 0;
+int receivedBytes = 0;
 
 void handleRoot() {
     server.send(200, "text/plain", "ESP32 EPD Server");
 }
 
-void handleSetLength() {
+void handleSetImageSize() {
     if (server.hasArg("plain") == false) {
         server.send(200, "text/plain", "Body not received");
         return;
     }
 
     String body = server.arg("plain");
-    encodedImageLength = body.toInt();
-    Serial.printf("Received length: %d\n", encodedImageLength);
+    imageSize = body.toInt();
+    Serial.printf("Received image size: %d\n", imageSize);
 
-    if (encodedImageLength <= 0) {
-        server.send(400, "text/plain", "Invalid length");
+    if (imageSize <= 0) {
+        server.send(400, "text/plain", "Invalid image size");
         return;
     }
 
-    if (encodedImage != nullptr) {
-        free(encodedImage);
+    if (image != nullptr) {
+        free(image);
     }
 
-    encodedImage = (unsigned char*)malloc(encodedImageLength);
-    if (encodedImage == nullptr) {
+    image = (unsigned char*)malloc(imageSize);
+    if (image == nullptr) {
         server.send(500, "text/plain", "Memory allocation failed");
         return;
     }
 
-    server.send(200, "text/plain", "Length received");
+    server.send(200, "text/plain", "Image size received");
+}
+
+void handleSetChunkSize() {
+    if (server.hasArg("plain") == false) {
+        server.send(200, "text/plain", "Body not received");
+        return;
+    }
+
+    String body = server.arg("plain");
+    chunkSize = body.toInt();
+    Serial.printf("Received chunk size: %d\n", chunkSize);
+
+    if (chunkSize <= 0) {
+        server.send(400, "text/plain", "Invalid chunk size");
+        return;
+    }
+
+    server.send(200, "text/plain", "Chunk size received");
 }
 
 void handleImageUpload() {
@@ -52,22 +72,44 @@ void handleImageUpload() {
         return;
     }
 
-    if (encodedImage == nullptr) {
-        server.send(400, "text/plain", "Length not set");
+    if (image == nullptr) {
+        server.send(400, "text/plain", "Image size not set");
+        return;
+    }
+
+    if (chunkSize == 0) {
+        server.send(400, "text/plain", "Chunk size not set");
         return;
     }
 
     String body = server.arg("plain");
-    memcpy(encodedImage, body.c_str(), encodedImageLength);
+    size_t bodyLength = body.length();
+
+    if (bodyLength < chunkSize && receivedBytes + bodyLength < imageSize) {
+        server.send(400, "text/plain", "Resend chunk");
+        return;
+    }
+    if (receivedBytes + bodyLength > imageSize) {
+        server.send(400, "text/plain", "Too much data");
+        return;
+    }
+
+    memcpy(image + receivedBytes, body.c_str(), bodyLength);
+    receivedBytes += bodyLength;
+
+    if (receivedBytes < imageSize) {
+        server.send(200, "text/plain", "Chunk received");
+        return;
+    }
 
     Serial.println("Image data received. Displaying image...");
 
     epdDisplay.init_fast();
-    epdDisplay.display(encodedImage, encodedImageLength);
+    epdDisplay.display(image, imageSize);
     epdDisplay.sleep();   
 
-    free(encodedImage);
-    encodedImage = nullptr;
+    free(image);
+    image = nullptr;
     server.send(200, "text/plain", "Image received and displayed");
 }
 
@@ -94,11 +136,15 @@ void setup() {
     Serial.print("Local ESP32 IP: ");
     Serial.println(WiFi.localIP());
 
+    Serial.println("Clearing Display");
     epdDisplay.init();
     epdDisplay.clear();
+    epdDisplay.sleep();
+    Serial.println("Cleared Display");
 
     server.on("/", HTTP_GET, handleRoot);
-    server.on("/setLength", HTTP_POST, handleSetLength);
+    server.on("/setSize", HTTP_POST, handleSetImageSize);
+    server.on("/setChunk", HTTP_POST, handleSetChunkSize);
     server.on("/upload", HTTP_POST, handleImageUpload);
 
     server.begin();
